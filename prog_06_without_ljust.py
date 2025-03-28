6a8742d28de0300c4d514b73d0b425a66dcfa93e
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 14:18:48 +0800

    Program 6

--- prog_06_without_ljust.py
+++ prog_06_without_ljust.py
@@ -1,3 +1,12 @@
 # get user input
+user_input = input("Enter the text: ")
+width_input = int(input("Enter the total width: "))
+
 # add spaces if the text is shorter than width
+if len(user_input) < width_input:
+    output = user_input + " " * (width_input - len(user_input))
+else:
+    output = user_input
+
 # print
+print(f"'{output}'")
