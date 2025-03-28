be8b68c24df11f0bc541a4a0f6837c8d6d62569f
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 12:22:23 +0800

    Program 1

--- prog_01_without_lstrip.py
+++ prog_01_without_lstrip.py
@@ -1,5 +1,15 @@
 # get user input
-# initialize
-# loop
+user_input = input("Enter your text: ")
+
+# initialize variable
+start = 0
+
+# loop until non-space character is found
+while start < len(user_input) and user_input[start] == " ":
+    start += 1
+
 # extract substring
+user_output = user_input[start:]
+
 # print
+print(user_output)
