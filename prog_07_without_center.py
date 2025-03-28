6db8ca07d7eb1437a19fc3e40d7a947693cf56e9
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 14:30:12 +0800

    Program 7

--- prog_07_without_center.py
+++ prog_07_without_center.py
@@ -1,5 +1,18 @@
 # get user input
+user_input = input("Enter the text: ")
+width_input = int(input("Enter the total width: "))
+
 # check if width is not smaller than length of text
-# calculate spaces to be add 
-# centralize the user input
+if width_input > len(user_input):    
+    # calculate spaces to be add 
+    total_spaces = width_input - len(user_input)
+    left_spaces =  total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    
+    # centralize the user input
+    output = " " * left_spaces + user_input + " " * right_spaces
+else:
+    output = user_input
+
 # print
+print(f"'{output}'")
