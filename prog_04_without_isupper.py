a06acdb510fba738aca268f7772b0cdafacf06ff
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 13:26:16 +0800

    Program 4

--- prog_04_without_isupper.py
+++ prog_04_without_isupper.py
@@ -1,4 +1,13 @@
 # get user input
+user_input = input("Enter your text: ")
+
 # initialize
+is_uppercase = True
+
 # loop through characters
+for character in user_input:
+    if "a" <= character <= "z":
+        is_uppercase = False
+        break
 # print
+print(is_uppercase)
