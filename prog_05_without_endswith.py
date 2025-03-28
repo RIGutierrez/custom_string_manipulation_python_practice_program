77c88fd654b1a854bfe4fb79da719d679ef8bcbc
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 13:52:41 +0800

    Program 5

--- prog_05_without_endswith.py
+++ prog_05_without_endswith.py
@@ -1,4 +1,13 @@
 # get user input and suffix to be checked
+text = input("Enter the text: ")
+suffix = input("Enter the suffix to check: ")
+
 # check suffix
-# remove last part of input and compare with suffix
-# print 
+if not suffix:
+    output = True
+else:
+    # remove last part of input and compare with suffix
+    output = text[-len(suffix):] == suffix
+
+# print
+print(output)
