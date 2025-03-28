38583ea04c41da9c074d275665789d69667d5575
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 12:51:12 +0800

    Program 2

--- prog_02_without_removeprefix.py
+++ prog_02_without_removeprefix.py
@@ -1,3 +1,12 @@
 # get user input
+user_input = input("Enter your text: ")
+remove_prefix = input("Enter the prefix to remove: ")
+
 # check if input start with the prefix
+if user_input.startswith(remove_prefix):
+    user_output = user_input.replace(remove_prefix, "", 1)
+else:
+    user_output = user_input
+
 # print
+print(user_output)
