05a6c6fe9b07b5b859e38210356e0cac55d4d7a1
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-28 14:44:53 +0800

    Program 8

--- prog_08_without_swapcase.py
+++ prog_08_without_swapcase.py
@@ -1,4 +1,17 @@
 # get user input
+user_input = input("Enter your text: ")
+
 # initialize
+output = ""
+
 # loop through characters
+for character in user_input:
+    if "A" <= character <= "Z":
+        output += chr(ord(character) + 32)
+    elif "a" <= character <= "z":
+        output += chr(ord(character) - 32)
+    else:
+        output += character
+
 # print
+print(output)
