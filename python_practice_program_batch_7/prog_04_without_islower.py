947a5e1842c363ebc11e814f9b788232831d8fce
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-30 21:51:05 +0800

    Program 4

--- python_practice_program_batch_7/prog_04_without_islower.py
+++ python_practice_program_batch_7/prog_04_without_islower.py
@@ -1,4 +1,14 @@
 # get user input
+user_input = input("Enter your text: ")
+
 # initialize
+is_lower = True
+
 # loop through characters
+for character in user_input:
+    if "A" <= character <= "Z":
+        is_lower = False
+        break  
+
 # print
+print(is_lower)
