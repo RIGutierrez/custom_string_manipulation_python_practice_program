502eeacf9f682ff72323ed5f83c2530f67c95403
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-30 21:31:46 +0800

    Program 2

--- python_practice_program_batch_7/prog_02_withput_removesuffix.py
+++ python_practice_program_batch_7/prog_02_withput_removesuffix.py
@@ -1,3 +1,12 @@
 # get user input
+user_input = input("Enter your text: ")
+remove_suffix = input("Enter the suffix to remove: ")
+
 # check if input ends with the suffix
+if user_input.endswith(remove_suffix):
+    output = user_input[:-len(remove_suffix)] 
+else:
+    output = user_input
+
 # print
+print(output)
