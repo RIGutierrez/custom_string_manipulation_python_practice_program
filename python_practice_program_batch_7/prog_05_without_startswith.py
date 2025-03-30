cdb494cb4ccc2e9db650d5b8033344f343bc6d7c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-30 21:58:32 +0800

    Program 5

--- python_practice_program_batch_7/prog_05_without_startswith.py
+++ python_practice_program_batch_7/prog_05_without_startswith.py
@@ -1,3 +1,13 @@
 # get user input and prefix to be checked
+user_input = input("Enter the text: ")
+prefix = input("Enter the prefix to check: ")
+
 # check prefix
+if not prefix:
+    output = True 
+else:
+    # remove first part of input and compare with prefix
+    output = user_input[:len(prefix)] == prefix
+
 # print
+print(output)
