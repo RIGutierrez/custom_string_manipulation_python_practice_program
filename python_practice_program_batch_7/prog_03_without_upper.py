20a98d59be688b5959aac2914bcfeff48f4d3298
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-30 21:43:35 +0800

    Program 3

--- python_practice_program_batch_7/prog_03_without_upper.py
+++ python_practice_program_batch_7/prog_03_without_upper.py
@@ -1,4 +1,15 @@
 # get user input
+user_input = input("Enter your text: ")
+
 # initialize
+uppercase_output = ""
+
 # loop through characters
+for character in user_input:
+    if "a" <= character <= "z":
+        uppercase_output += chr(ord(character) - 32)
+    else:
+        uppercase_output += character
+
 # print
+print(uppercase_output)
