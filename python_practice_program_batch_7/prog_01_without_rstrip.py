133c4aa025256d39e5495f855718110d0eb1b982
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-30 21:23:25 +0800

    Program 1

--- python_practice_program_batch_7/prog_01_without_rstrip.py
+++ python_practice_program_batch_7/prog_01_without_rstrip.py
@@ -1,5 +1,15 @@
 # get user input
+user_input = input("Enter your text: ")
+
 # initialize value from last character
+start = len(user_input) - 1
+
 # loop until non-space character is found (right side)
+while start >= 0 and user_input[start] == " ":
+    start -= 1
+
 # extract substring
+user_output = user_input[:start + 1]
+
 # print
+print(user_output)
