866990d303edc4e1a4623d566348b8f728a84ca4
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 14:22:49 +0800

    Program 10

--- python_practice_program_batch_7/prog_10_without_rindex.py
+++ python_practice_program_batch_7/prog_10_without_rindex.py
@@ -1,4 +1,15 @@
 # get user input and character/text to find
+user_input = input("Enter your text: ")
+target_character = input("Enter the character to find: ")
+
 # loop through characters and check if the character/text to be find exists (reverse)
-# check if character/text matches
-# print
+for i in range(len(user_input) - len(target_character), -1, -1):
+    
+    # check if character/text matches
+    if user_input[i:i+len(target_character)] == target_character:
+        
+        # print
+        print(i)
+        break
+else:
+    print("Target character/text not found")
