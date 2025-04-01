4916ae4483595b594cb31fd653ba51c1c08b8644
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 13:41:34 +0800

    Program 8

--- python_practice_program_batch_7/prog_08_without_count.py
+++ python_practice_program_batch_7/prog_08_without_count.py
@@ -1,4 +1,16 @@
 # get user input and text to be counted in text
+user_input = input("Enter your text: ")
+text_to_count = input("Enter the text to count: ")
+
 # initialize
+count = 0
+index = 0
+
 # loop through characters
+while index <= len(user_input) - len(text_to_count):
+    if user_input[index : index + len(text_to_count)] == text_to_count:
+        count += 1
+    index += 1
+
 # print
+print(count)
