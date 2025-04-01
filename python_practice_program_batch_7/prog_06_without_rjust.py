30aecaf35c2d988890cad6541991d59e2bef86d2
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 13:12:17 +0800

    Program 6

--- python_practice_program_batch_7/prog_06_without_rjust.py
+++ python_practice_program_batch_7/prog_06_without_rjust.py
@@ -1,3 +1,12 @@
 # get user input
-# add spaes if the text is shorter than width
+user_input = input("Enter the text: ")
+width_input = int(input("Enter the total width: "))
+
+# add spaces if the text is shorter than width
+if len(user_input) < width_input:
+    output = " " * (width_input - len(user_input)) + user_input  
+else:
+    output = user_input
+
 # print
+print(f"'{output}'")
