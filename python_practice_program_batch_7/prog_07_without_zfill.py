90a9e521a78769889587813d980090216d6064d9
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-04-01 13:18:54 +0800

    Program 7

--- python_practice_program_batch_7/prog_07_without_zfill.py
+++ python_practice_program_batch_7/prog_07_without_zfill.py
@@ -1,3 +1,12 @@
 # get user input
+user_input = input("Enter the text: ")
+width_input = int(input("Enter the total width: "))
+
 # add zero if the text is shorter than width
+if len(user_input) < width_input:
+    output = "0" * (width_input - len(user_input)) + user_input 
+else: 
+    output = user_input
+
 # print
+print(f"'{output}'")
